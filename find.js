eaf58c7b809ad4f2e919e663550c0adec82bd792
Sandy <a0919613087@gmail.com>
2022-08-01 01:10:12 +0800

    add find.js

---
+++ find.js
@@ -0,0 +1,34 @@
+let ary = [
+    {
+      id: 1,
+      name: 'Mia',
+      color: 'orange',
+    },
+    {
+      id: 2,
+      name: 'John',
+      color: 'yellow',
+    },
+    {
+      id: 3,
+      name: 'Eva',
+      color: 'orange',
+    },
+];
+  
+// 找出 第一筆喜歡orange的人
+  
+//find()
+let result = ary.find((item) => item.color === 'orange')
+console.log(result); 
+//Mia like orange
+  
+  
+//for-loop
+function find(ary) {
+  for (let i = 0; i < ary.length; i++) {
+    return ary[i];
+  }
+}
+console.log(find(ary));
+//Mia like orange
