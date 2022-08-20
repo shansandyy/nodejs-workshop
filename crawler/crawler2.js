16d7a1b1966b575dec51bd3b310c34be6824feec
Sandy <a0919613087@gmail.com>
2022-08-20 11:43:15 +0800

    update await

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -2,14 +2,21 @@
 //更好的參數設定
 
 const axios = require('axios');
+
 let stockNo = 2330;
 let queryDate = '20220814';
 
-axios
-    .get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220813&stockNo=2330&_=1660378514253')
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+    (async ()=>{
+        try{
+            let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY`,{
+                params: {
+                    response: 'json',
+                    date: queryDate,
+                    stockNo: stockNo,
+                },
+            });
+            console.log(response.data);
+        }catch (e) {
+            console.error(e);
+        }
+    })()
