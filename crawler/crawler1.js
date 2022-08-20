51cd9acd1e3826397cb2b8be549ab6489ce56cf9
Sandy <a0919613087@gmail.com>
2022-08-20 11:16:17 +0800

    update 註解

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -1,8 +1,15 @@
+//1.下載npm i axios
+//2.require 引用
+
+//3.開始抓資料
 const axios = require('axios');
 axios
     .get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220813&stockNo=2330&_=1660378514253')
     .then((response) => {
-        console.log(response);
+        // console.log(response);
+
+        //抓取data
+        console.log(response.data);
     })
     .catch((error) => {
         console.error(error);
