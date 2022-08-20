ee4930715dfdfd3b6bddf8d764bcacceadc15b10
Sandy <a0919613087@gmail.com>
2022-08-20 13:39:52 +0800

    update 註解

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -4,14 +4,11 @@ const axios = require('axios');
 const moment = require('moment');
 const fs = require('fs/promises');
 
-
-
 (async () => {
   try {
     let stockNo = await fs.readFile('stock.txt', 'utf8');
-    let queryDate = moment().format('YYYYMMDD'); //'20220814';
     
-    // https://www.twse.com.tw/zh/api/codeQuery?query=2330
+    //https://www.twse.com.tw/zh/api/codeQuery?query=2330
     let queryNameResponse = await axios.get(` https://www.twse.com.tw/zh/api/codeQuery`, {
       params: {
         query: stockNo
@@ -19,16 +16,26 @@ const fs = require('fs/promises');
     });
     let suggestions = queryNameResponse.data.suggestions;
     let suggestion = suggestions[0];
-    // console.log('first', suggestion);
-    if (suggestion === '(無符合之代碼或名稱)') {
-        console.error(suggestion);
-        throw new error(suggestion);
+    // console.log('suggestions.first',suggestion);
+    
+    if(suggestion === "(無符合之代碼或名稱)") {
+      console.error(suggestion);
+      throw new Error(suggestion);
     }
-    let stockName = suggestion.split('\t').pop();
-    // console.log(stockName[1]);
-    console.log(stockName);
 
-    // console.log(queryNameResponse.data);
+    //透過 split() 轉為陣列, pop()取得最後一筆 --> 用 [1] 若陣列改變容易取得錯誤資料 
+    let stockName = suggestion.split('\t').pop();
+    console.log('stockName', stockName);
+
+    let queryDate = moment().format('YYYYMMDD'); //'20220814';
+    let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY`, {
+      params: {
+        response: 'json',
+        date: queryDate,
+        stockNo: stockNo,
+      }
+    })
+    console.log(response.data);
   } catch (e) {
     console.error(e);
   }
